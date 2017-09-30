348066e5025fb5e656d25efb7d15542bb2957d8f
Hovhannes Hakobyan <hhakobyan@live.com>
2017-09-30 17:46:42 +0400

    Web API

--- StoreSample/App_Start/RouteConfig.cs
+++ StoreSample/App_Start/RouteConfig.cs
@@ -7,17 +7,20 @@ using System.Web.Routing;
 
 namespace StoreSample
 {
+    using System.Web.Http;
+
     public class RouteConfig
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapHttpRoute(name: "DefaultApi", routeTemplate: "api/{controller}/{id}", defaults: new { id = RouteParameter.Optional });
+            
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional });
         }
     }
 }
--- StoreSample/Controllers/HomeController.cs
+++ StoreSample/Controllers/HomeController.cs
@@ -1,30 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace StoreSample.Controllers
+﻿namespace StoreSample.Controllers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(int? id)
         {
-            return View();
-        }
+            this.ViewBag.Categories = "";
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return this.View(id);
         }
     }
 }
