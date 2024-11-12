55a4d5f0e56e3230eb4b6befa8a963b2974a7095
Hovhannes HAKOBYAN <hovhannes.hakobyan@amadeus.com>
2024-11-12 12:18:00 +0400

    Update dependencies

--- StoreSample/Entities/StoreEntities.Designer.cs
+++ StoreSample/Entities/StoreEntities.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\hhako\Documents\Visual Studio 2017\Projects\StoreSample\StoreSample\Entities\StoreEntities.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\hhakobyan\Source\Repos\hhakobyan\StoreSample\StoreSample\Entities\StoreEntities.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
